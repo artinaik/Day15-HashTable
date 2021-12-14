33bda6077055507c15f05103229d8556c6dcd710
Your UserName <aratinaik151997@gmail.com>
2021-12-14 15:11:47 +0530

    Finding frequency of words for sentence

--- HashTable/HashTable/Program.cs
+++ HashTable/HashTable/Program.cs
@@ -11,6 +11,18 @@ namespace HashTable
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to HashTable Project");
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(5);
+            hash.Add(0, "to");
+            hash.Add(1, "be");
+            hash.Add(2, "or");
+            hash.Add(3, "not");
+            hash.Add(4, "to");
+            hash.Add(5, "be");
+            Console.WriteLine("Frequency of words in sentence are :");
+            hash.FindFrequency("to");
+            hash.FindFrequency("be");
+            hash.FindFrequency("or");
+            hash.FindFrequency("not");
             Console.ReadKey();
         }
     }
